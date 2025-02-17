edff38fc5b7590a0e203de9e4959a097aae4feec
Kateryna <katiaemme96@yahoo.it>
2025-02-17 15:28:58 +0100

    Inizializzazione cartelle e autenticazioni

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { HeaderComponent } from './main-components/header/header.component';
+import { FooterComponent } from './main-components/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/interfaces/i-user.ts
@@ -0,0 +1,12 @@
+export enum Role {
+  ORGANIZER = ' ROLE_ORGANIZER',
+  USER = 'ROLE_USER',
+}
+
+export interface iUser {
+  id?: number;
+  username: string;
+  email: string;
+  password: string;
+  role: Role;
+}
